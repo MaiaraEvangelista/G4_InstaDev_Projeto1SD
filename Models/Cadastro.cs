ec1beff6b08c7ceef74c430bf2574d3d231a533f
sjvinicius <73194624+sjvinicius@users.noreply.github.com>
2021-02-01 16:32:04 -0300

    site de login aparecendo, porém sem funcionalidades

---
+++ Models/Cadastro.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System.IO;
+using G4_InstaDev_Projeto1SD.Interfaces;
+
+namespace G4_InstaDev_Projeto1SD.Models
+{
+    public class Cadastro : CadastroBase, ICadastro 
+    {
+        public string Email { get; set; }
+        public string Nome { get; set; }
+        public string Username { get; set; }
+        public string Senha { get; set; }
+
+        private const string PATH = "Database/Cadastro.csv";
+
+        public string Prepare (Cadastro c){
+            return $"{c.Email}, {c.Nome}, {c.Username}, {c.Senha}";
+        }
+        public Cadastro (){
+            CreateFolderAndFile(PATH);
+        }
+
+        public void Create(Cadastro c)
+        {
+            string [] linhas = {Prepare(c)};
+            File.AppendAllLines(PATH, linhas);
+        }
+
+        public List<Cadastro> ReadAll()
+        {
+            List<Cadastro> cadastros = new List<Cadastro> ();
+            string [] linhas = File.ReadAllLines(PATH);
+
+            foreach (var item in linhas)
+            {
+                string [] linha = item.Split (";");
+                Cadastro cadastro = new Cadastro ();
+
+                cadastro.Email    = linha[0];
+                cadastro.Nome     = linha[1];
+                cadastro.Username = linha[2];
+                cadastro.Senha    = linha[3];
+
+                cadastros.Add(cadastro);
+
+            }
+            return cadastros;
+        }
+
+        public void Remove(string username)
+        {
+            List<string> linhas = ReadAllLinesCSV(PATH);
+            linhas.RemoveAll(x=> x.Split(";")[0] == username.ToString());
+            RewriteCSV (PATH, linhas);
+        }
+
+        public void Uptade(Cadastro c)
+        {
+            List<string> linhas = ReadAllLinesCSV(PATH);
+            linhas.RemoveAll(x=> x.Split(";")[0] == c.Username.ToString());
+            linhas.Add(Prepare (c));
+            RewriteCSV (PATH, linhas);
+
+        }
+    }
+}
--- Models/Login.cs
+++ Models/Login.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace G4_InstaDev_Projeto1SD.Models
 {
     public class Login
@@ -17,34 +19,41 @@ namespace G4_InstaDev_Projeto1SD.Models
         private const string PATH = "Database/Cadastro.csv";
 
         
-        public string ValidacaoEmaileSenha(string EmailDigitado, string email, string SenhaDigitada, string senha){
+        public bool ValidacaoEmaileSenha(string EmailDigitado, string SenhaDigitada ){
+            
+            //  Instanciar um objeto 
 
             //  Coletar as informações presentes no CSV de cadastro
-            string linhas = ReadAllLines(PATH);
+            string [] linhas = File.ReadAllLines(PATH);
 
             //  Percorrer as linhas do CSV
-            foreach (var linha in linhas)
+            foreach (var dado in linhas)
             {
                 
                 //  Atribuir o valor do email correspondente no CSV a uma váriavel para fazer a comparação
-                string [] email = linha.Split(";")[0];
+                string emailCSV = dado.Split(";")[0];
 
                 //  Atribuir o valor da senha correspondedo no CSV a uma váriavel para fazer a comparação
-                string [] senha = linha.split(";")[3];
+                string senhaCSV = dado.Split(";")[3];
 
                 // verificar se o email E senha digitado correspondem a algum email e senha presente na mesma Linha
-                if (email == EmailDigitado && senha == SenhaDigitada )
+                if (emailCSV == EmailDigitado && senhaCSV == SenhaDigitada )
                 {
                     
                     //  Se ele encontrar retorna o valor verdadeiro
-                    Validacao == "Login Localizado";
+                    Validacao = "Login Localizado";
 
+
+                    return true;
+                    
                 }
 
             }
 
             //  Se não encontrar retorna falso, e a utilização desses dados serão representados no cshtml
-            Validacao == "Email ou Senha Incorretos, caso";
+            Validacao = "Email ou Senha Incorretos, caso";
+
+            return false;
 
         }   
         
