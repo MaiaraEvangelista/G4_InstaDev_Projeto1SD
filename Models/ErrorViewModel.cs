eb477375fb32090c2f11b564cad37b6aea046723
Giovanni Andrade Nespoli <73205603+GiovanniNespoli@users.noreply.github.com>
2021-02-02 21:47:50 -0300

    perfil

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace G4_InstaDev_Projeto1SD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
