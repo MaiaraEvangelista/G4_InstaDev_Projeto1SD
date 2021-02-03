0edc001bcfc2f8de2add9d08ef6b15c6c9b2657a
Giovanni Andrade Nespoli <73205603+GiovanniNespoli@users.noreply.github.com>
2021-02-03 14:41:52 -0300

    develop

--- Models/Perfil.cs
+++ Models/Perfil.cs
@@ -7,8 +7,21 @@ namespace G4_InstaDev_Projeto1SD.Models
 {
     public class Perfil : SuperClass, IPerfil
     {
+        Cadastro cadastro = new Cadastro();
+
         public int IdComentarios { get; set; }
         public string Coments { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public string User { get; set; }
+
+        public void pass()
+        {
+            cadastro.Nome = Name;
+            cadastro.Username = User;
+            cadastro.Imagem = Img;
+        }
+
         private const string PATH = "DataBase/Perfil.csv";
 
         public Perfil()
@@ -18,32 +31,33 @@ namespace G4_InstaDev_Projeto1SD.Models
 
         public string linha(Perfil y)
         {
-            return $"{y.Coments};{y.IdComentarios};";
+                return $"{y.Name};{y.User};{y.Img};{y.Coments};{y.IdComentarios};";
         }
         public void Create(Perfil x)
         {
             var lista = ReadAll();
 
-            if(lista.Count > 0)
-            {   
+            if (lista.Count > 0)
+            {
                 Random numale = new Random();
                 int numint = numale.Next();
 
                 x.IdComentarios = numint;
             }
-            else{
-                
+            else
+            {
+
                 x.IdComentarios = 1;
             }
 
             string[] lin = { linha(x) };
-            File.AppendAllLines(PATH,lin);
+            File.AppendAllLines(PATH, lin);
         }
 
         public void Delete(int id)
         {
             List<string> linhas = ReadAllLinesCSV(PATH);
-            linhas.RemoveAll(x => x.Split(";")[1] == id.ToString());
+            linhas.RemoveAll(x => x.Split(";")[4] == id.ToString());
             RewriteCSV(PATH, linhas);
         }
 
@@ -52,15 +66,18 @@ namespace G4_InstaDev_Projeto1SD.Models
             List<Perfil> perf = new List<Perfil>();
             string[] ler = File.ReadAllLines(PATH);
 
-            foreach(var item in ler)
+            foreach (var item in ler)
             {
                 string[] sep = item.Split(';');
 
                 Perfil perfil = new Perfil();
-                perfil.Coments = sep[0];
-                perfil.IdComentarios = int.Parse(sep[1]);
+                perfil.Name = sep[0];
+                perfil.User = sep[1];
+                perfil.Img = sep[2];
+                perfil.Coments = sep[3];
+                perfil.IdComentarios = int.Parse(sep[4]);
 
-                perf.Add(perfil);    
+                perf.Add(perfil);
             }
 
             return perf;
