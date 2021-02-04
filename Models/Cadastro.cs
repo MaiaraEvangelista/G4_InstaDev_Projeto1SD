502d869875a311010f5246b6e031d588d1806716
bielvdm <gabrielvdm2010@hotmail.com>
2021-02-04 15:21:07 -0300

    id cadastro add

--- Models/Cadastro.cs
+++ Models/Cadastro.cs
@@ -10,13 +10,14 @@ namespace G4_InstaDev_Projeto1SD.Models
         public string Nome { get; set; }
         public string Username { get; set; }
         public string Senha { get; set; }
+        public int IdCadastro { get; set; }
         private string imagem = "padrao.png";
         public string Imagem { get {return imagem;} set{imagem = value;}}
 
         public const string PATH = "Database/Cadastro.csv";
 
         public string Prepare (Cadastro c){
-            return $"{c.Email};{c.Nome};{c.Username};{c.Senha};{c.Imagem}";
+            return $"{c.IdCadastro};{c.Email};{c.Nome};{c.Username};{c.Senha};{c.Imagem}";
         }
         public Cadastro (){
             CreateFileAndFolder(PATH);
@@ -24,6 +25,19 @@ namespace G4_InstaDev_Projeto1SD.Models
 
         public void Create(Cadastro c)
         {
+            var lista = ReadAll();
+
+            if (lista.Count > 0)
+            {
+                Random num = new Random();
+                int nume = num.Next();
+                c.IdCadastro = nume;
+            }
+            else
+            {
+                c.IdCadastro = 1;
+            }
+
             string [] linhas = {Prepare(c)};
             File.AppendAllLines(PATH, linhas);
         }
